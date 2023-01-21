8795245f7254f69e76ccf37ab4b94c552e71478d
Marc Roma <marc.roma@cirvianum.cat>
2023-01-21 02:42:29 +0100

    Codi completat -> Funció Comprovar, LlistarRetirats | Missatges per pantalla, etc.

--- Caixa-Blanca/src/caixa/blanca/vehicles.java
+++ Caixa-Blanca/src/caixa/blanca/vehicles.java
@@ -12,16 +12,16 @@ public class vehicles {
 
     static String[][] vehiclesALL = { // TOTS ELS VEHICLES
         /*{MATRÍCULA, ANY MATRICULACIÓ, TIPUS CARBURANT}*/
-        {"1234BCD", "2004", "GASOLINA"},
-        {"5678ABC", "2000", "GASOIL"},
-        {"9012GFH", "2017", "GASOIL"},
+        {"1234BCD", "2004", "DIESEL"},
+        {"5678ABC", "2001", "GASOLINA"},
+        {"9012GFH", "2017", "DIESEL"},
         {"1035YUI", "1998", "GASOLINA"},
         {"2987POP", "2020", "GASOLINA"}
     };
 
-    static String[][] vehiclesRetirats = null;
+    static String[][] vehiclesRetirats;
 
-    static void LlistarTots() { // LLISTAR TOTS ELS VEHÍCLES
+    public static void LlistarTots() { // LLISTAR TOTS ELS VEHÍCLES
 
         System.out.println("LLISTAR TOTS ELS VEHICLES:");
         System.out.println("---------");
@@ -37,45 +37,49 @@ public class vehicles {
         }
     }
 
-    static void Comprovar() {
-        boolean marcador = false;
+    public static void Comprovar() {
         int comptador = 0;
-        int anyVehicle;
 
+        // CONTAR EL NOMBRE DE VEHICLES QUE S'ON ABANS DEL 2010 I SON DIESEL
         for (int i = 0; i < vehiclesALL.length; i++) {
-            // CONVERTIR EL VALOR DE LA POSICIÓ ON HI HA ELS ANYS
-            // DE MATRICULACIÓ DE STRING A INT PER POGUER SER 
-            // COMPARATS.
-            anyVehicle = Integer.parseInt(vehiclesALL[i][1]);
-
-            if (anyVehicle < 2010) {
-                marcador = true;
-                comptador = comptador + 1;
+            int anyVehicle = Integer.parseInt(vehiclesALL[i][1]);
+            if (anyVehicle < 2010 && vehiclesALL[i][2].equals("DIESEL")) {
+                comptador++;
+                System.out.println(comptador);
             }
         }
+        // INICIALITZAR vehiclesRetirats AMB LA LLARGADA NECESSARIA EPR EMMAGATZEMAR-HI ELS VEHICLES TROBATS
+        vehiclesRetirats = new String[comptador][3];
+        comptador = 0;
 
+        // AFEGIR ELS VEHICLES ESMENTATS ANTERIORMENT A L'ARRAY vehiclesRetirats
         for (int i = 0; i < vehiclesALL.length; i++) {
-            if (marcador == true && vehiclesALL[i][2] == "GASOIL") {
-
-                // AFEGIR VEHICLE A ARRAY VEHICLES RETIRATS
+            int anyVehicle = Integer.parseInt(vehiclesALL[i][1]);
+            if (anyVehicle < 2010 && vehiclesALL[i][2].equals("DIESEL")) {
+                for (int j = 0; j < 3; j++) {
+                    vehiclesRetirats[comptador][j] = vehiclesALL[i][j];
+                }
+                comptador++;
+                
             }
         }
+        comptador = 0;
+        
     }
 
-    static void LlistarRetirats() { // LLISTAR NOMÉS ELS VEHICLES RETIRATS
-        System.out.println("LLISTAR ELS VEHICLES RETIRATS:");
-        System.out.println("---------");
-
-        for (int i = 0; i < vehiclesRetirats.length; i++) {
-            System.out.println("VEHICLE N. " + i + " :");
-
-            for (int j = 0; j < vehiclesRetirats[i].length; j++) {
-                System.out.println(vehiclesRetirats[i][j]);
-            }
+    public static void LlistarRetirats() { // LLISTAR NOMÉS ELS VEHICLES RETIRATS
+        if (vehiclesRetirats == null) {
+            System.out.println("Encara no hi ha cap vehicle retirat a la llista.");
+        } else {
+            System.out.println("LLISTAR ELS VEHICLES RETIRATS:");
             System.out.println("---------");
-            System.out.println(" ");
+            for (int i = 0; i < vehiclesRetirats.length; i++) {
+                for (int j = 0; j < vehiclesRetirats[i].length; j++) {
+                    System.out.print(vehiclesRetirats[i][j] + " ");
+                }
+                System.out.println();
+            }
         }
 
     }
-
 }
